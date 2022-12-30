15a01358e8c34f159b1aa593dabd8470b426817b
Artur Lipskii <falcon.arthur@gmail.com>
2022-12-30 21:48:39 +0200

    searchbar work

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,27 @@
+import { useState } from "react";
+
 function SearchBar({ onSubmit }) {
+  const [term, setTerm] = useState("");
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(term);
+  };
+  const handleChange = (event) => {
+    setTerm(event.target.value)
 
-
+  };
   return (
     <div>
-      <form action=""><input type="text" placeholder="Search" name="" id="" /></form>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          placeholder="Search"
+          name="searchbar"
+          id="searchbar"
+          value={term}
+          onChange={handleChange}
+        />
+      </form>
     </div>
   );
 }
